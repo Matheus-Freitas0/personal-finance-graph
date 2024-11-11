c41111d4f3fa355f6cca0c5a2791a69148586267
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-11 14:38:19 -0300

    first commit

---
+++ src/entrypoints/hello.ts
@@ -0,0 +1,22 @@
+import { reasonStream } from 'tryreason'
+
+interface City {
+  /** A two sentence description of the city */
+  description: string;  
+  points_of_interest: {
+    name: string;
+    description: string;
+    address: string;
+  }[];
+}
+
+// POST /hello
+export async function* POST(req: Request) {
+  const res = await fetch(`http://ip-api.com/json/`)
+  if (res.status !== 200) {
+    return new Response('Error', { status: 500 })
+  }
+  const { city } = await res.json()
+
+  return reasonStream<City>(`Tell me about ${city}`)
+}
